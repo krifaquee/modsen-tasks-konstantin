5e03806e5c572c5e28031072949dc190fbc7a03c
krifaquee <borowik.kostya@gmail.com>
2025-06-26 16:06:45 +0300

    ReviewTask2

---
+++ app/src/main/java/com/example/modsentaskskonstantin/di/dataModule.kt
@@ -0,0 +1,15 @@
+package com.example.modsentaskskonstantin.di
+
+import com.example.modsentaskskonstantin.data.TaskRepositoryImpl
+import com.example.modsentaskskonstantin.data.auth.AuthRepositoryImpl
+import com.example.modsentaskskonstantin.data.repository.PostRepositoryImpl
+import com.example.modsentaskskonstantin.domain.repository.IAuthRepository
+import com.example.modsentaskskonstantin.domain.repository.IPostRepository
+import com.example.modsentaskskonstantin.domain.repository.ITaskRepository
+import org.koin.dsl.module
+
+val dataModule = module {
+    single<ITaskRepository> { TaskRepositoryImpl() }
+    single<IAuthRepository> { AuthRepositoryImpl() }
+    single<IPostRepository> { PostRepositoryImpl(get()) }
+}
---
+++ app/src/main/java/com/example/modsentaskskonstantin/domain/model/PostDomainModel.kt
@@ -0,0 +1,8 @@
+package com.example.modsentaskskonstantin.domain.model
+
+data class PostDomainModel(
+    val userId: Int,
+    val id: Int,
+    val title: String,
+    val body: String
+)
